695883a800e0c36d7c9540f0898c4ac294399ffe
Alessandro Vullo <avullo@ebi.ac.uk>
2017-05-30 22:43:44 +0100

    Transformed into a script, no functions to call.

--- run_analysis.R
+++ run_analysis.R
@@ -1,68 +1,49 @@
+#!/usr/bin/env Rscript
+
 ## run_analysis.R
-##  
+
+## assume Samsung data is in working directory
+data_dir <- "UCI HAR Dataset"
+
+## 1. Merge the training and the test sets to create one data set
+
+# For the training/test sets, create a data frame with the data 
+# set, labels and subject identifier paste together
+
+data_set <- read.table(sprintf("%s/train/X_train.txt", data_dir))
+training_labels <- read.table(sprintf("%s/train/y_train.txt", data_dir))
+training_subjects <- read.table(sprintf("%s/train/subject_train.txt", data_dir))
+
+data_set <- cbind(data_set, training_subjects, training_labels)
+rm(training_labels, training_subjects) # some cleaning, not needed
+
+test_set <- read.table(sprintf("%s/test/X_test.txt", data_dir))
+test_labels <- read.table(sprintf("%s/test/y_test.txt", data_dir))
+test_subjects <- read.table(sprintf("%s/test/subject_test.txt", data_dir))
+
+# Append test set with features/subjects/labels
+data_set <- rbind(data_set, cbind(test_set, test_subjects, test_labels))
+
+## 2. Extract only the measurements on the mean and standard deviation for each measurement
+# Read the file with the descriptions of the input features
+feature_names <- read.table(sprintf("%s/features.txt", data_dir), sep = " ")
+
+# Return the data frame with only the mean/std measurements, plus
+# subject index and output label
+data_set <- data_set[, c(grep("mean|std", feature_names[,2]), 562, 563)]
+
+## 3. Use descriptive activity names to name the activities in the data set
+activities <- read.table(sprintf("%s/activity_labels.txt", data_dir), sep = " ")
+
+# Get the vector of lower case activity labels
+activities <- tolower(activities[,2])
+
+# Add a column of descriptive activity names
+data_set$activity <- activities[data[,ncol(data)]]
+
+## HERE
+## 4. Appropriately labels the data set with descriptive variable names. 
 
 
-## merge_data_sets
-## Merges the training and the test sets to create one data set
 
-merge_data_sets <- function(directory) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the 'activity_labels.txt file which
-  ## maps numeric indices to activity names
-  
-  ## For the training/test sets, create a data frame with the data 
-  ## set, labels and subject identifier paste together
-  
-  data_set <- read.table(sprintf("%s/train/X_train.txt", directory))
-  training_labels <- read.table(sprintf("%s/train/y_train.txt", directory))
-  training_subjects <- read.table(sprintf("%s/train/subject_train.txt", directory))
- 
-  data_set <- cbind(data_set, training_subjects, training_labels)
-  rm(training_labels, training_subjects) # some cleaning, not needed
-  
-  test_set <- read.table(sprintf("%s/test/X_test.txt", directory))
-  test_labels <- read.table(sprintf("%s/test/y_test.txt", directory))
-  test_subjects <- read.table(sprintf("%s/test/subject_test.txt", directory))
-  
-  ## Append test set with features/subjects/labels
-  data_set <- rbind(data_set, cbind(test_set, test_subjects, test_labels))
-  
-  data_set
-}
-
-## filter_measurements
-## For the merged data set obtained in step 1, extracts only the measurements on 
-## the mean and standard deviation for each measurement
-
-filter_measurements <- function(data, directory) {
-  ## 'data' is the data frame obtained by merging the training and test
-  ## set together
-  
-  ## 'directory' is is a character vector of length 1 indicating
-  ## the location of the 'features.txt' file which maps numeric
-  ## indices to the names of the features in the input vector
-  
-  ## Read the file with the descriptions of the input features
-  feature_names <- read.table(sprintf("%s/features.txt"), sep = " ")
-  
-  ## Return the data frame with only the mean/std measurements, plus
-  ## subject index and output label
-  data[, c(grep("mean|std", feature_names[,2]), 562, 563)]
-}
-
-## get_activity_names
-## Return a vector of descriptive activity names (lower case)
-
-get_activity_names <- function(directory) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the 'activity_labels.txt file which
-  ## maps numeric indices to activity names
-
-  ## Read files with activity labels
-  activities <- read.table(sprintf("%s/activity_labels.txt", directory), sep = " ")
-  
-  ## Return a vector of lower case activity labels
-  tolower(activities[,2])
-}
-
-## 
+# write.table(data_set, file="dataset.csv", row.names=FALSE)
